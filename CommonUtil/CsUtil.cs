5cdd7592549afd4753e955a8082c51138a040b9a
gms1071 <gms1071@gepplus.co.kr>
2016-11-09 10:45:45 +0900

    Test Vs2012 CommonUtil

---
+++ CommonUtil/CsUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonUtil
+{
+	public static class CsUtil
+	{
+		public static int ConStr_Int32(string input)
+		{
+			return ConStr_Int32(input, 0);
+		}
+
+		public static int ConStr_Int32(this string input, Int32 defValue)
+		{
+			Int32 iReturn = 0;
+
+			if (!Int32.TryParse(input, out iReturn))
+			{
+				iReturn = defValue;
+			}
+
+			return iReturn;
+		}
+
+	}
+}
